c99b9332e0b3218bfcf24563e6acc4e84efd1070
miguelbenevides22 <infenexd@gmail.com>
2026-09-06 23:14:02 -0300

    Refactor Vite config to update plugins and paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
-import viteReact from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   cssMinify: false,
+
   resolve: {
-    tsconfigPaths: true,
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
+
   plugins: [
-    tsconfigPaths(),
-    tanstackStart(),
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
     nitro(),
-    viteReact(),
+    react(),
     tailwindcss(),
   ],
 });
